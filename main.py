c2978e292a9da5088a8abda920e33cd1855c9b05
Kuwex <vizinovskiy@gmail.com>
2026-02-21 15:05:27 +0300

    add main.py and get endpoint

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, UploadFile
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+    
+@app.get("/full_files")
+async def file_names(pswd: str):
+    if pswd == "hashpass":
+        return {"access"}
+
+@app.get("/file/{name}")
+async def get_file(name: str):
+    return FileResponse(f"./public_files/{name}", filename=f"{name}")
